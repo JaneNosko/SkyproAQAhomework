c18b563bf227ca7af2d17f1c3f6ab3a605a3c1d7
JaneNosko <noskojane12@gmail.com>
2023-08-08 01:26:46 +0900

    создали новый файл

---
+++ lesson_1_task_8.py
@@ -0,0 +1,15 @@
+def number(num): 
+   print(num)
+# Вызовите эту функцию 11 раз так, чтобы в консоль вывелся номер 88005553535
+
+number(8)
+number(8)
+number(0)
+number(0)
+number(5)
+number(5)
+number(5)
+number(3)
+number(5)
+number(3)
+number(5)
