9411575950f6c4bb1f035b98e422fe209a7615c1
JaneNosko <noskojane12@gmail.com>
2023-08-08 01:25:20 +0900

    создали новый файл

---
+++ lesson_1_task_7.py
@@ -0,0 +1,43 @@
+def console_1():
+    print("1")
+
+def console_2():
+    print("2")
+
+def console_3():
+    print("3")
+
+def console_4():
+    print("4")
+
+def console_5():
+    print("5")
+
+def console_6():
+    print("6")
+
+def console_7():
+    print("7")
+
+def console_8():
+    print("8")
+
+def console_9():
+    print("9")
+
+def console_0():
+    print("0")
+
+# Вызовите объявленные функции в таком порядке, чтобы на экране отобразился номер 88005553535
+
+console_8()
+console_8()
+console_0()
+console_0()
+console_5()
+console_5()
+console_5()
+console_3()
+console_5()
+console_3()
+console_5()
